8f05269e92741bb37c0f9ea25b94646e05201a19
Sibin George <sibingeorge009@gmail.com>
2024-07-19 01:22:35 -0400

    implemented employee filter based on employee type

--- server/graphql/resolvers/resolvers.js
+++ server/graphql/resolvers/resolvers.js
@@ -1,4 +1,3 @@
-
 import { } from "../../models/db.js";
 import { Employee } from "../../models/schema.js";
 import { GQLDate } from "./sacalars.js";
@@ -6,24 +5,17 @@ import { GQLDate } from "./sacalars.js";
 
 export const resolvers = {
     Query: {
-        getEmployees: async () =>{
-            const employees = await Employee.find({});
+        getEmployees: async (_, { type }) => {
+            const filter = type ? { employeeType: type } : {};
+            const employees = await Employee.find(filter);
             return employees;
-        } 
+        },
     },
     Mutation: {
-        addEmployee: async (_, {employee}) => {
-        //   try {
-        //     const newEmployee = new Employee(args);
-        //     return await newEmployee.save();
-        //   } catch (error) {
-        //     console.error('Error adding employee:', error);
-        //     throw new Error('Error adding employee');
-        //   }
-
-             Employee.create(employee);
-             return employee;
+        addEmployee: async (_, { employee }) => {
+            Employee.create(employee);
+            return employee;
         },
-      },
+    },
     Date: GQLDate
 };
